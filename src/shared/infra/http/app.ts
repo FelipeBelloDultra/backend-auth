613238528715ca6fed578a57342a2eaddd23a813
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2023-02-12 14:23:21 -0300

    feat: add cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,11 +1,13 @@
 // Packages
 import express from "express";
+import cors from "cors";
 
 // Routes
 import { apiRouter } from "./routes";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/api", apiRouter);
 
