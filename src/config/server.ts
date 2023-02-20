889ed1e563c6f71f7dc27245b4929b139919fe1f
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2023-02-20 13:59:39 -0300

    config: get app port from .env

---
+++ src/config/server.ts
@@ -0,0 +1,5 @@
+const serverPort = process.env.SERVER_PORT || 8080;
+
+export default {
+  port: serverPort,
+};
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
+// App instance
 import { app } from "@/shared/infra/http/app";
 
-app.listen(3001, () => {
-  console.log("Server running \n");
+// Configs
+import serverConfig from "@/config/server";
+
+app.listen(serverConfig.port, () => {
+  console.log(`Server running on ${serverConfig.port} \n`);
 });
