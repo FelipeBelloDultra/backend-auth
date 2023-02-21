86fce97c00139c16b449a3f256755d837eab4f0d
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2023-02-21 20:47:55 -0300

    test: fix factories methods

--- tests/factories/index.ts
+++ tests/factories/index.ts
@@ -4,12 +4,14 @@ import { faker } from "@faker-js/faker";
 // Entities
 import { ICreateUserDTO } from "@/modules/users/dtos/create-user-dto";
 
-export function createUserFactory(): ICreateUserDTO {
-  const name = faker.name.fullName();
+export function createEmailFactory(): string {
+  return faker.internet.email();
+}
 
+export function createUserFactory(): ICreateUserDTO {
   return {
-    name,
-    email: faker.internet.email(),
+    name: faker.name.fullName(),
+    email: createEmailFactory(),
     password: faker.internet.password(),
   };
 }
