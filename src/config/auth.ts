da4f95a2e48cdaad576391b4a37417d819c8c846
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2023-02-21 23:24:29 -0300

    fix(config): change config file name

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,4 +1,4 @@
 export default {
   secret: process.env.APP_JWT_AUTH_CREDENTIAL || "default",
-  expiresIn: process.env.APP_JWT_AUTH_EXPIRES_IN || "1d",
+  expiresIn: process.env.APP_JWT_AUTH_EXPIRES_IN || "1h",
 };
