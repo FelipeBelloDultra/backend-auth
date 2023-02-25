13da2b3d2a3c3704512672e3262bd365462248d2
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2023-02-24 21:22:35 -0300

    test(E2E/users): implment test to update user

--- tests/e2e/users/update-user-e2e.spec.ts
+++ tests/e2e/users/update-user-e2e.spec.ts
@@ -45,4 +45,37 @@ describe("[PUT] - Update user", () => {
     expect(response.body.data.email).toBe(newEmail);
     expect(response.body.data.name).toBe(createdUser.name);
   });
+
+  it("should not be able to update user without data", async () => {
+    const { body } = authenticatedUser;
+
+    const response = await supertest(app)
+      .put(BASE_URL)
+      .send({
+        name: createdUser.name,
+        email: createdUser.email,
+        username: "",
+        password: createdUser.password,
+      })
+      .set("Authorization", `Bearer ${body.data.token}`);
+
+    expect(response.status).toBe(422);
+    expect(response.body).toHaveProperty("data", null);
+    expect(response.body.error).toHaveProperty("errors");
+    expect(response.body.error.errors).toHaveProperty("username");
+  });
+
+  it("should not be able to update user with wrong beraer token", async () => {
+    const response = await supertest(app)
+      .put(BASE_URL)
+      .send({
+        name: createdUser.name,
+        email: createdUser.email,
+        username: fakeUser.createUserNameFactory(),
+        password: createdUser.password,
+      })
+      .set("Authorization", "Bearer WRONG-BEARER-TOKEN");
+
+    expect(response.status).toBe(401);
+  });
 });
